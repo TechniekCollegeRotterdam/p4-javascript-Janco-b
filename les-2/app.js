26729037552ef06930081827fd437b3a968b3004
janco buitendijk <9016333@student.zadkine.nl>
2022-05-20 12:10:20 +0200

    arrow keys

--- les-2/app.js
+++ les-2/app.js
@@ -1,7 +1,28 @@
+// const bird = document.querySelector(".bird");
+// let offsetleft = 0;
+// bird.addEventListener("click", function (){
+//         offsetleft += 20;
+// //zorgt ervoor dat de vird 20 pixels naar links
+//         bird.style.left = offsetleft+ "px";
+// });
+
 const bird = document.querySelector(".bird");
-let offsetleft = 0;
-bird.addEventListener("click", function (){
-        offsetleft += 20;
-//zorgt ervoor dat de vird 20 pixels naar links gaat
-        bird.style.left = offsetleft+ "px";
+let count = 0;
+window.addEventListener("keydown", function (e){
+    if(e.key == "ArrowRight"){
+        count += 20;
+        bird.style.left = count + "px";
+    }
+    if(e.key == "ArrowLeft"){
+        count -= 20;
+        bird.style.left = count + "px";
+    }
+    if(e.key == "ArrowUp"){
+        count += 20;
+        bird.style.bottom = count + "px";
+    }
+    if(e.key == "ArrowDown"){
+        count += 20;
+        bird.style.top = count + "px";
+    }
 });
